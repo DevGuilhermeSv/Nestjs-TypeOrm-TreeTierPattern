bb6f9c98d5805ee26f10a03bc657711fd2207dda
Guilherme <gui.2469@gmail.com>
2023-04-19 16:13:18 -0300

    removed baserepository

--- src/Repository/base.interface.repository.ts
+++
@@ -1,8 +0,0 @@
-import { DeleteResult } from 'typeorm';
-
-export interface IBaseRepository<T> {
-  create(data: T | any): Promise<T>;
-  delete(id: string): Promise<DeleteResult>;
-  findAll(): Promise<T[]>;
-  findById(id: number): Promise<T>;
-}
